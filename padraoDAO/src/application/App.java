0356daae51c9d446e9a991956d35fabdde813fbc
Kauan Ferreira <kfr@academico.ufpb.br>
2024-02-04 13:10:57 -0300

    Classe Seller

--- padraoDAO/src/application/App.java
+++ padraoDAO/src/application/App.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department department = new Department(1, "Computers");
-        System.out.println(department);
+        Seller seller = new Seller(1, "Dudu", "dudu@gmail.com", new Date(), 1500d, department);
+        System.out.println(seller);
     }
 }
